7a6389d0595218f94f39bc96cbed4859322c3f71
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-08-31 17:31:42 +0530

    commit to resolve issue #2

--- bot/pipeline/scraping.py
+++ bot/pipeline/scraping.py
@@ -1,7 +1,5 @@
-import os
-
-# from bot import config
 import requests
+import re
 import pandas as pd
 from typing import List
 from bs4 import BeautifulSoup
@@ -15,18 +13,45 @@ class GetLinks:
         resp = requests.get(url=self.url)
         return resp.text
 
-    def extract_links(self) -> List[str]:
-        body = self.make_request()
+    def extract_links(self, body: str) -> List[str]:
         soup = BeautifulSoup(body, "html.parser")
         links = [link.get("href") for link in soup.find_all("a")]
+        if len(links) < 1:
+            raise ValueError("No links found.")
         return links
+
+    def save_links(self, links: List[str]) -> str:
+        link_dataframe = pd.DataFrame(links, columns=["Links"])
+        if len(link_dataframe) <= 1:
+            raise ValueError("Trying to create an empty dataframe.")
+
+        url_pattern = re.compile(
+            r"^(https?://)?"
+            r"(www\.)?"
+            r"([a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+)"
+            r"(/[a-zA-Z0-9._~:/?#@!$&\'()*+,;=%-]*)?$"
+        )
+        links_filtered = link_dataframe[
+            link_dataframe["Links"].apply(lambda x: bool(url_pattern.match(x)))
+        ]
+
+        try:
+            links_filtered.to_csv(r"bot\data\links\links.csv", index=False)
+            print("Successfully saved dataframe.")
+        except IOError:
+            print("Failed to save dataframe.")
+
+    def run_pipline(self):
+        body = self.make_request()
+        links = self.extract_links(body=body)
+        self.save_links(links=links)
+        print("Pipeline ran successfully.")
 
 
 def main() -> None:
-    get_links = GetLinks("https://doj.gov.in/")
-    links = get_links.extract_links()
-    link_dataframe = pd.DataFrame(links, columns=["Links"])
-    link_dataframe.to_csv("data/links/links.csv", index=False)
+    URL = "https://doj.gov.in/"
+    get_links = GetLinks(url=URL)
+    get_links.run_pipline()
 
 
 if __name__ == "__main__":
