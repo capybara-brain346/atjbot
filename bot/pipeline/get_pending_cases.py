c0f833bca87db42b5161a7e27453b1f7af1c1936
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-09-02 16:25:38 +0530

    removed list import

--- bot/pipeline/get_pending_cases.py
+++ bot/pipeline/get_pending_cases.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from typing import Dict, List
+from typing import Dict
 from bs4 import BeautifulSoup
 
 
