6c63705921c18b80c1101d95d9a1b501d569d84a
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-09-03 22:36:21 +0530

    implemented part of speech tagging for links retrieval #11

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,12 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
+import json
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk import pos_tag
 from flask_cors import CORS
 
 load_dotenv()
@@ -19,6 +25,30 @@ app = Flask(
 logging.basicConfig(level=logging.INFO)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+def extract_keywords(prompt):
+    nltk.download("punkt", quiet=True)
+    nltk.download("punkt_tab", quiet=True)
+    nltk.download("stopwords", quiet=True)
+    nltk.download("averaged_perceptron_tagger", quiet=True)
+    nltk.download("averaged_perceptron_tagger_eng", quiet=True)
+
+    tokens = word_tokenize(prompt.lower())
+
+    stop_words = set(stopwords.words("english"))
+    filtered_tokens = [
+        word for word in tokens if word.isalnum() and word not in stop_words
+    ]
+
+    tagged_tokens = pos_tag(filtered_tokens)
+
+    keywords = [
+        word for word, tag in tagged_tokens if tag in ("NN", "NNS", "JJ", "JJR", "JJS")
+    ]
+    fdist = FreqDist(keywords)
+
+    return fdist.most_common()
 
 
 def query_rag(query_text: str, prompt_template: str) -> str:
@@ -50,12 +80,23 @@ def query_rag(query_text: str, prompt_template: str) -> str:
         return "An error occurred while processing the query."
 
 
-def get_links():
-    links = [
-        "http://legislative.gov.in/",
-        "https://nja.gov.in/",
-        "https://limbs.gov.in/",
-    ]
+def get_links(prompt):
+    keywords = extract_keywords(prompt)
+    keyword_joint = " ".join([keyword for keyword, idx in keywords]).title()
+
+    json_file_path = r"bot\data\links\links.json"
+
+    with open(json_file_path, "r") as j:
+        contents = json.loads(j.read())
+
+    dict_content = dict(contents)
+    dict_keys = dict_content.keys()
+
+    links = []
+    for i in dict_keys:
+        if keyword_joint in i:
+            links.append(dict_content[i])
+
     return links
 
 
@@ -83,7 +124,7 @@ def predict():
         Answer the question based on the above context in 50 to 80 words. If the question is not related to legal content respond with I cannot help with this query. You can be a little interactive by replying to simple prompts like greetings and goodbyes: {question}
         """
         response = query_rag(query_text=message, prompt_template=PROMPT_TEMPLATE)
-        return jsonify({"answer": response, "links": get_links()})
+        return jsonify({"answer": response, "links": get_links(message)})
 
     except Exception as e:
         logging.error(f"Error in /predict: {e}")
