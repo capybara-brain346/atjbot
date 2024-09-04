549656c692abd3bcd8d02b488094188e85b95496
AaryanPalve5 <aspalve2004@gmail.com>
2024-09-04 14:17:13 +0530

    link bubbles prototype 1 added #9

--- frontend/react-chatbot/src/ChatApp.js
+++ frontend/react-chatbot/src/ChatApp.js
@@ -1,25 +1,33 @@
-// src/ChatApp.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
 import './index.css';
+
 const ChatApp = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(true);
+
+  const suggestions = [
+    'What is tele law?',
+    'What is Department of Justice?'
+  ];
 
   const toggleChatbox = () => {
     setIsVisible(!isVisible);
   };
 
-  const sendMessage = () => {
-    if (!message) return;
+  const sendMessage = (text) => {
+    const msg = text || message;
+    if (!msg) return;
 
-    const newMessage = { text: message, isUser: true };
+    const newMessage = { text: msg, isUser: true };
     setMessages([newMessage, ...messages]);
     setMessage('');
+    setShowSuggestions(false); // Hide suggestions after a message is sent
 
-    axios.post('http://127.0.0.1:5000/predict', { message })
+    axios.post('http://127.0.0.1:5000/predict', { message: msg })
       .then(response => {
         const answer = response.data.answer;
         const links = response.data.links;
@@ -34,6 +42,10 @@ const ChatApp = () => {
         const errorMessage = { text: 'An error occurred while processing your request.', isUser: false };
         setMessages([errorMessage, ...messages]);
       });
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    sendMessage(suggestion);
   };
 
   return (
@@ -55,6 +67,20 @@ const ChatApp = () => {
                 {msg.isLink ? <a href={msg.text} target="_blank" rel="noopener noreferrer">{msg.text}</a> : msg.text}
               </div>
             ))}
+            {/* Suggestions as part of message area */}
+            {showSuggestions && (
+              <div className="flex flex-col items-center justify-center gap-2 mt-4">
+                {suggestions.map((suggestion, index) => (
+                  <button
+                    key={index}
+                    className="p-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 w-3/4 text-center"
+                    onClick={() => handleSuggestionClick(suggestion)}
+                  >
+                    {suggestion}
+                  </button>
+                ))}
+              </div>
+            )}
           </div>
           <div className="bg-gradient-to-r from-yellow-800 to-yellow-500 p-5 flex items-center justify-between rounded-b-lg shadow-md">
             <input
@@ -64,7 +90,7 @@ const ChatApp = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
-            <button className="p-1.5 bg-transparent border-none cursor-pointer" onClick={sendMessage}>
+            <button className="p-1.5 bg-transparent border-none cursor-pointer" onClick={() => sendMessage()}>
               Send
             </button>
           </div>
