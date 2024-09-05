e3b8aac224f280267ec5a977188cd8cfb43d61d5
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-09-05 22:31:22 +0530

    updated bashfile for vite

--- start_project.bat
+++ start_project.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Start the frontend
-start cmd /c "cd frontend\react-chatbot && npm start"
+start cmd /c "cd frontend\react+vite && npm run dev"
 
 :: Start the backend
 python backend\app.py
